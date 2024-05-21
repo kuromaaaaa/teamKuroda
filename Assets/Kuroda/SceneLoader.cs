77fc27074a8258687fe62135deac6bfd9c8bbf10
kuromaaaaa <kuromaxa@gmail.com>
2024-05-21 15:39:21 +0900

    使うかもしれないスクリプト(Triggerの三つとCollision三つをUnityEventで呼ぶ)とTimaManagerを追加

---
+++ Assets/Kuroda/SceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    /// <summary>
+    /// シーンをロードするやつ UnityEventなどで呼んで
+    /// </summary>
+    /// <param name="sceneName"></param>
+    public void Load(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
