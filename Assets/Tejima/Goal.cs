562f06684753771767f5a80f462b7aee387ea236
tejimatsuyoshi <tejima.tuyoshi@icloud.com>
2024-05-21 11:49:19 +0900

    それぞれのシーンに飛ぶためのスクリプトの作成

---
+++ Assets/Tejima/Goal.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            SceneManager.LoadScene("Title", LoadSceneMode.Single);
+        }
+    }
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
+    }
+}
