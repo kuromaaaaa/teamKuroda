d16365a40f05d8556d26e0128f4f17b394875b47
kuromaaaaa <kuromaxa@gmail.com>
2024-05-22 17:38:24 +0900

    ver2

--- Assets/Kuroda/AnimationSet.cs
+++ Assets/Kuroda/AnimationSet.cs
@@ -6,16 +6,22 @@ public class AnimationSet: MonoBehaviour
 {
     Animator _anim;
     Rigidbody2D _rb;
+    SpriteRenderer _sp;
     // Start is called before the first frame update
     void Start()
     {
         _anim = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
+        _sp = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(_rb.velocity.x > 0)
+            _sp.flipX = false;
+        if (_rb.velocity.x < 0)
+            _sp.flipX = true;
         _anim.SetFloat("velX", Mathf.Abs(_rb.velocity.x));
         _anim.SetFloat("velY", _rb.velocity.y);
     }
